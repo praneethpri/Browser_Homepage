b9f1d8bad494a8494c4e5eaf224ca39737ecf8c9
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-07-16 08:35:37 +0530

    adding wallpaper component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WallpaperComponent } from './components/wallpaper/wallpaper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WallpaperComponent
   ],
   imports: [
     BrowserModule
