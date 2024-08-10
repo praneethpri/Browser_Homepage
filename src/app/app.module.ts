330a0fee583fffc3984e43c8a040b5b3feb620a1
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-08-10 14:39:51 +0530

    adding search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TitleLimiterPipe } from './pipes/title-limiter.pipe';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { TitleLimiterPipe } from './pipes/title-limiter.pipe';
     ControlButtonsComponent,
     AddLinkComponent,
     TitleLimiterPipe,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
